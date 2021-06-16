6a351768398661c797574b565dd6c21819620239
IwonaDobrut <iwona.dobrut2@gmail.com>
2021-06-16 15:11:58 +0200

    IwonaTest2

--- src/test/java/itacademy/pizzastore/service/OrderServiceTest.java
+++ src/test/java/itacademy/pizzastore/service/OrderServiceTest.java
@@ -35,9 +35,22 @@ class OrderServiceTest {
     public void getPaymentTypeReturnPaymentType() {
         Pizza two = new Pizza(1, Size.LARGE, "2", BigDecimal.ONE, List.of(), List.of());
         Order order = new Order(124, List.of(two), null, "223-456-789", BigDecimal.ONE, Status.PAYED, 25, PaymentType.CARD);
+        OrderRepository.save(order);
 
-        Order result = orderService.getPaymentType(124);
+        order result = orderService.getStatusForOrder(124, PaymentType.CARD);
         assertThat(result).isEqualTo(PaymentType.CARD);
+    }
+
+    @Test
+
+    public void getDeliveryTimeInMinutes() {
+        Pizza two = new Pizza(1, Size.LARGE, "2", BigDecimal.ONE, List.of(), List.of());
+        Order order = new Order(124, List.of(two), null, "223-456-789", BigDecimal.ONE, Status.PAYED, 25, PaymentType.CARD);
+        OrderRepository.save(order);
+
+        Order result = orderService.getStatusForOrder(124, getDeliveryTimeInMinutes(25));
+        assertThat(result).isEqualTo(25);
+
     }
 
 
